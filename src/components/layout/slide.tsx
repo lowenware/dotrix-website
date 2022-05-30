316c85bc7a012b83aa378da4c7270ba23912c47d
Ilja Kartašov <ik@lowenware.com>
2022-05-30 23:00:32 +0200

    Optimize Slide component

--- src/components/layout/slide.tsx
+++ src/components/layout/slide.tsx
@@ -1,59 +1,46 @@
 import classNames from "classnames";
 import React, {ReactNode} from "react";
 
-type SlideSizeEnum = "small" | "medium" | "large";
+type SlideSizeEnum = "small" | "large";
 
 interface SlideProps {
   children?: ReactNode,
   image: string,
   size?: SlideSizeEnum,
+  className?: string,
 }
 
 export const Slide: React.FC<SlideProps> = ({
+  className,
   image,
   children,
   size = "small",
 }) => {
-  const stretch = "absolute top-0 left-0 right-0 bottom-0";
-  return (
-    <>
-      {size === "large" && (
-        <div
-          className="h-80vh sm:h-screen overflow-hidden
-              bg-no-repeat sm:bg-cover bg-fixed relative
-            bg-auto"
-          style={{backgroundImage: `url(${image})`}}
-        >
-          <div
-            className={classNames(stretch, "h-screen bg-dark opacity-75")}
-          ></div>
-          <div
-            className={classNames("flex justify-center items-center", stretch)}
-          >
-            {children}
-          </div>
-        </div>
-      )}
 
-      {size === "small" && (
-        <div
-          className="relative h-30vh overflow-hidden bg-fixed
-         bg-no-repeat sm:bg-cover top-80 bg-auto"
-          style={{backgroundImage: `url(${image})`}}
-        >
-          <div
-            className={classNames(stretch, "h-screen bg-dark opacity-75")}
-          ></div>
-          <div
-            className={classNames(
-              "flex justify-center items-center z-30",
-              stretch
-            )}
-          >
-            {children}
-          </div>
-        </div>
-      )}
-    </>
+  const stretch = "absolute top-0 left-0 right-0 bottom-0";
+  const sizeClassNames = (() => {
+    switch (size) {
+      case "small": return "h-30vh";
+      case "large": return "h-80vh sm:h-screen pt-80";
+    }
+  })();
+
+  return (
+    <div
+      style={{backgroundImage: `url(${image})`}}
+      className={
+        classNames(
+          `slide--${size}`,
+          className,
+          sizeClassNames,
+          "overflow-hidden bg-no-repeat sm:bg-cover bg-fixed relative bg-auto"
+        )
+      }
+    >
+      <div className={classNames(stretch, "bg-dark opacity-90")}></div>
+      <div className={classNames("flex justify-center items-center", stretch)}>
+        {children}
+      </div>
+    </div>
   );
 };
