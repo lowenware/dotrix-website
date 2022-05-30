29595b97fd4959454ff0482d736adc7e33139589
Danilo Kachanko <dk@lowenware.com>
2022-05-30 15:56:49 +0200

    Fix Footer Component

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -18,7 +18,7 @@ const Community: NextPage = () => {
               Community
             </p>
             </Slide>
-        <section className="w-full relative z-50">
+        <section className="w-full relative z-40">
           <div className="p-32 pt-0 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-32 -mt-32">
           <CommunityCard
             className=""
