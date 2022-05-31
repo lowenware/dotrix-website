4f31afef059df160d45f2f564aac75280a91d1be
Ilja Kartašov <ik@lowenware.com>
2022-05-31 16:43:36 +0200

    Resolve linting warnings

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,29 +1,32 @@
-import ArrowLeft from "assets/arrow-left.svg"
-import ArrowRight from "assets/arrow-right.svg"
-import LogoDiscord from "assets/logo-discord.svg"
-import LogoDotrix from "assets/logo-dotrix.svg"
-import LogoGithub from "assets/logo-github.svg"
-import LogoLowenware from "assets/logo-lowenware.svg"
-import LogoPatreon from "assets/logo-patreon.svg"
-import LogoTwitter from "assets/logo-twitter.svg"
-import LogoYoutube from "assets/logo-youtube.svg"
-import MenuIcon from "assets/menu-icon.svg"
+/* eslint-disable no-restricted-imports */
+import ArrowLeft from "assets/arrow-left.svg";
+import ArrowRight from "assets/arrow-right.svg";
+import LogoDiscord from "assets/logo-discord.svg";
+import LogoDotrix from "assets/logo-dotrix.svg";
+import LogoGithub from "assets/logo-github.svg";
+import LogoLowenware from "assets/logo-lowenware.svg";
+import LogoPatreon from "assets/logo-patreon.svg";
+import LogoTwitter from "assets/logo-twitter.svg";
+import LogoYoutube from "assets/logo-youtube.svg";
+import MenuIcon from "assets/menu-icon.svg";
+/* eslint-enable no-restricted-imports */
+import Image from "next/image";
 
 export const Arrow = {
-  Left: () => <img src={ArrowLeft} />,
-  Right: () => <img src={ArrowRight} />,
+  Left: () => <Image src={ArrowLeft} alt="" />,
+  Right: () => <Image src={ArrowRight} alt="" />,
 };
 
 export const Logo = {
-  Discord: () => <img src={LogoDiscord} />,
-  Dotrix: () => <img src={LogoDotrix} />,
-  Github: () => <img src={LogoGithub} />,
-  Lowenware: () => <img src={LogoLowenware} />,
-  Patreon: () => <img src={LogoPatreon} />,
-  Twitter: () => <img src={LogoTwitter} />,
-  Youtube: () => <img src={LogoYoutube} />,
+  Discord: () => <Image src={LogoDiscord} alt="" />,
+  Dotrix: () => <Image src={LogoDotrix} alt="" />,
+  Github: () => <Image src={LogoGithub} alt="" />,
+  Lowenware: () => <Image src={LogoLowenware} alt="" />,
+  Patreon: () => <Image src={LogoPatreon} alt="" />,
+  Twitter: () => <Image src={LogoTwitter} alt="" />,
+  Youtube: () => <Image src={LogoYoutube} alt="" />,
 };
 
 export const Icon = {
-  Menu: () => <img src={MenuIcon} />,
+  Menu: () => <Image src={MenuIcon} alt="" />,
 };
