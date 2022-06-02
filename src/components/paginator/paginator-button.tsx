77f1bb4877220b1755310b57cc6923dd04d6642a
Ilja Kartašov <ik@lowenware.com>
2022-06-02 09:58:37 +0200

    Update paginator layout

--- src/components/paginator/paginator-button.tsx
+++ src/components/paginator/paginator-button.tsx
@@ -1,64 +1,49 @@
-import {CSSProperties} from "react";
+import classNames from "classnames";
 
-const paginatorButtonStyle: CSSProperties = {
-  padding: "8px 15px",
-  textAlign: "center",
-  borderRadius: 4,
-  color: "#88919B",
-  fontWeight: 700,
-  margin: 2,
-};
-const currentPaginatorButton = {
-  ...paginatorButtonStyle,
+import {Button} from "~/components/button";
 
-  background: "#11131A",
-  color: "#0FB7FF",
-};
 export enum PaginatorButtonType {
   Normal,
   PrevPage,
   NextPage,
   Placeholder,
 }
+
 interface PaginatorButtonProps {
   linkType: PaginatorButtonType,
   pageNumber?: number,
   link: string,
   isCurrent: boolean,
 }
+
 export const PaginatorButton: React.FC<PaginatorButtonProps> = ({
   linkType,
   pageNumber,
   link,
   isCurrent,
 }) => {
-  if (pageNumber) {
-    if (linkType == PaginatorButtonType.PrevPage) {
-      return (
-        <a className="hidden sm:block" href={link} style={paginatorButtonStyle}>
-          {"<"}
-        </a>
-      );
-    }
-    if (linkType == PaginatorButtonType.Normal) {
-      return (
-        <a
-          href={link}
-          style={isCurrent ? currentPaginatorButton : paginatorButtonStyle}
-        >
-          {pageNumber}
-        </a>
-      );
-    } else if (linkType == PaginatorButtonType.NextPage) {
-      return (
-        <a className="hidden sm:block" href={link} style={paginatorButtonStyle}>
-          {">"}
-        </a>
-      );
+  if (!pageNumber) {
+    if (linkType == PaginatorButtonType.Placeholder) {
+      return <span className="text-white">{"..."}</span>;
     }
   }
-  if (linkType == PaginatorButtonType.Placeholder) {
-    return <span className="text-gray">{"..."}</span>;
-  }
-  return null;
+
+  const [buttonText, className] = (() => {
+    switch (linkType) {
+    case PaginatorButtonType.PrevPage: return [<>&lt;</>, "text-medium hidden sm:flex"];
+    case PaginatorButtonType.NextPage: return [<>&gt;</>, "text-medium hidden sm:flex"];
+    default: return [`${pageNumber}`, "text-small"];
+    }
+  })();
+
+  return (
+    <Button
+      variant={isCurrent ? "primary" : "outline"}
+      href={link}
+      className={classNames(className, "py-0 px-0 w-10 h-10")}
+    >
+      {buttonText}
+    </Button>
+  );
+
 };
